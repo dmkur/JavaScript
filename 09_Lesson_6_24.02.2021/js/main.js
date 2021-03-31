ca906b264ca7cadfdf8da13840a71bbaf7e6600f
dmkur <fgameua@gmail.com>
2021-03-31 22:23:41 +0300

    edit 6 les

--- 09_Lesson_6_24.02.2021/js/main.js
+++ 09_Lesson_6_24.02.2021/js/main.js
@@ -69,31 +69,155 @@
 
 // Все робити через функції масивів (foreach, map ...тд)
 // Дан масив :
-let users = [ 
-    {name: 'vasya', age: 31, status: false}, 
-    {name: 'petya', age: 30, status: true}, 
-    {name: 'kolya', age: 29, status: true}, 
-    {name: 'olya', age: 28, status: false}, 
-    {name: 'max', age: 30, status: true}, 
-    {name: 'anya', age: 31, status: false}, 
-    {name: 'oleg', age: 28, status: false}, 
-    {name: 'andrey', age: 29, status: true}, 
-    {name: 'masha', age: 30, status: true}, 
-    {name: 'olya', age: 31, status: false}, 
-    {name: 'max', age: 31, status: true} 
-];
+// let users = [ 
+//     {name: 'vasya', age: 31, status: false}, 
+//     {name: 'petya', age: 30, status: true}, 
+//     {name: 'kolya', age: 29, status: true}, 
+//     {name: 'olya', age: 28, status: false}, 
+//     {name: 'max', age: 30, status: true}, 
+//     {name: 'anya', age: 31, status: false}, 
+//     {name: 'oleg', age: 28, status: false}, 
+//     {name: 'andrey', age: 29, status: true}, 
+//     {name: 'masha', age: 30, status: true}, 
+//     {name: 'olya', age: 31, status: false}, 
+//     {name: 'max', age: 31, status: true} 
+// ];
 // - відсортувати його за  віком (зростання , а потім окремо спадання)
-users.sort((a,b) => a.age - b.age)
-console.log(users);
 
-// - відсортувати його за кількістю знаків в імені  (зростання , а потім окремо спадання)
-// - пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний індентифікатор (По якому принципу його створювати - ваше рішення), та зберегти це в новий масив (первинний масив залишиться без змін)
+// users.sort((a,b) => a.age - b.age)
+// console.log(users);
+
+// - відсортувати його за кількістю знаків в імені  
+//(зростання , а потім окремо спадання)
+
+// users.sort((a,b) => a.name.length - b.name.length)
+// console.log(users);
+
+// - пройтись по ньому та додати кожному юзеру поле id - яке характеризує 
+//унікальний індентифікатор (По якому принципу його створювати - ваше рішення), 
+//та зберегти це в новий масив (первинний масив залишиться без змін)
 // - відсортувати його за індентифікатором
+// console.log(users);
+// let b = JSON.parse(JSON.stringify(users))
 
+// let a = b.map((value,index) => {
+//    value.id = index * Math.random()
+//    return value
+// })
+
+// console.log(a);
+// a.sort((a,b => a.id - b.id))
+// console.log(a);
 
 
 
 
 
 // -- наисать функцию калькулятора с 2мя числами и колбеком
+// let calc = (a,b, callback) => callback(a,b)
+
+
+// let b = calc(3,3,((a,b) => a+b+a))
+// console.log(b);
+
+
 // -- наисать функцию калькулятора с 3мя числами и колбеком
+// let calc2 = (a,b,c, callback) => callback(a,b,c)
+
+// let c = calc2(20,50,20,(f,g,h) => f+g-h)
+// console.log(c);
+
+// function calc3(a,b,c,lol) {
+//     return lol(a,b,c,)
+// }
+
+
+// console.log(calc3(1,2,3,((a,b,c) => a+b+c-a)));
+
+
+// =============================================
+// =============================================
+// =============КЛАССНАЯ РАБОТА=================
+// =============================================
+// =============================================
+
+
+
+
+let cars = [ 
+    {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, 
+    {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250}, 
+    {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300}, 
+    {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140}, 
+    {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200}, 
+    {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165},
+    {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120}, 
+    {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120}, 
+    {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350}, 
+    {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180}, 
+    {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180}, 
+    {producer:"mercedes",model: "e63",year: 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400}, 
+    {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
+// Відфільтрувати масив за наступними крітеріями :
+// - двигун більше 3х літрів
+// let a = cars.filter(value => value.volume > 2)
+// console.log(a);
+
+// - двигун = 2л
+// let a = cars.filter(value => value.volume = 2)
+// console.log(a);
+
+
+// - виробник мерс
+// let a = cars.filter(value => value.producer === `mercedes`)
+// console.log(a);
+
+
+// - двигун більше 3х літрів + виробник мерседес
+// let a = cars.filter(value => value.producer === `mercedes` && value.volume >= 3)
+// console.log(a);
+
+// - двигун більше 3х літрів + виробник субару
+// let a = cars.filter(value => value.producer === `subaru` && value.volume >= 3)
+// console.log(a);
+
+// - сили більше ніж 300
+// let a = cars.filter(value => value.power > 300)
+// console.log(a);
+
+// - сили більше ніж 300 + виробник субару
+// let a = cars.filter(value => value.power > 300 && value.producer === `subaru`)
+// console.log(a);
+
+// - мотор серіі ej
+// let a = cars.filter(value => value.engine.startsWith(`ej`))
+// console.log(a);
+
+
+// - сили більше ніж 300 + виробник субару + мотор серіі ej
+// let a = cars.filter(
+//     value => value.power > 300 && 
+//     value.producer === `subaru` && 
+//     value.engine.startsWith(`ej`)
+//     )
+//     console.log(a);
+
+// - двигун меньше 3х літрів + виробник мерседес
+// let b = cars.filter(valie => valie.volume < 3 && valie.producer === `mercedes`)
+// console.log(b);
+
+// - двигун більше 2л + сили більше 250
+// let a = cars.filter(a => a.volume > 2 && a.power >300)
+// console.log(a);
+
+// - сили більше 250  + виробник бмв
+let b = cars.filter(a => a.power > 250 && a.producer === `bmw`)
+console.log(b);
+
+
+
+
+
+
+
+
