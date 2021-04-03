97bbf2cd4ed7625e8da000a2cfc59202073839dd
dmkur <fgameua@gmail.com>
2021-04-03 11:57:52 +0300

    les 7

--- 10_Lesson_7_26.02.2021/js/main.js
+++ 10_Lesson_7_26.02.2021/js/main.js
@@ -1,1 +1,120 @@
-alert(`lol`)
+// - Создать произвольный елемент с id = text.  
+//Используя JavaScript, сделайте так, чтобы при клике на 
+//кнопку исчезал элемент с id="text".
+
+// let btnElement = document.getElementById(`btn`)
+// let txtElement = document.getElementById(`text`)
+//txtElement.style.display = `block`
+
+// btnElement.onclick = item => {   
+    
+//     // txtElement.style.display === `block`
+//     // ? txtElement.style.display = `none`
+//     // : txtElement.style.display = `block`
+//     txtElement.hidden
+//     ?txtElement.hidden = false
+//     :txtElement.hidden = true
+// }
+
+//ще раз
+
+// let div = document.createElement(`div`)
+// let btn = document.createElement(`button`)
+
+// div.innerText = `lorem Lorem lorem Lorem lorem Lorem `
+// div.style.border = `1px solid blue`
+
+// btn.innerText = `Hide Me`
+// btn.style.background = `blue`
+// btn.style.color = `white`
+
+// document.body.appendChild(div) 
+// document.body.appendChild(btn) 
+
+
+// btn.onclick = event => {
+//     div.hidden
+//     ? div.hidden = false
+//     : div.hidden = true
+//}
+
+
+
+// - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+//let btnElement = document.getElementById(`btn`)
+// btnElement.onclick = item => {
+//     btnElement.style.display = `none`    
+// }
+
+// let btnElement = document.getElementById(`btn`)
+// btnElement.onclick = event => {
+//     event.target.style.display = `none`
+// }
+
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+//При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік 
+//чи меньше він ніж 18, та повідомити про це користувача
+
+
+
+
+// - Создайте меню, которое раскрывается/сворачивается при клике
+
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+// Вывести список комментариев в документ, каждый в своем блоке.
+//  Добавьте каждому комментарию по кнопке для сворачивания его body.
+
+// - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та виводиться на консоль інформація з цих 2х форм.
+// Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
+// Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
+
+// - Створити функцію, яка генерує таблицю.
+// Перший аргумент визначає кількість строк.
+// Другий параметр визначає кліькіть ячеєк в кожній строці.
+// Третій параметр визначає елемент в який потрібно таблицю додати.
+
+
+// - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+// При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+// (Додатковачастина для завдання)
+
+// - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
+
+
+// - Сворити масив не цензцрних слів.
+// Сворити інпут текстового типу.
+// Якщо людина вводить слово і воно міститься в масиві не цензурних слів
+// кинути алерт з попередженням.
+// Перевірку робити при натисканні на кнопку
+
+
+// - Сворити масив не цензцрних слів.
+// Сворити інпут текстового типу.
+// Потрібно перевіряти чи не містить ціле речення в собі погані слова.
+// Кинути алерт з попередженням у випадку якщо містить.
+// Перевірку робити при натисканні на кнопку
+
+
+
+// -- создать скрипт, который берет считывает на странице (rules.html) текст и делает сбоку меню-оглавление по всем заголовкам которые есть в тексте.
+// При клике на пункт оглавления вы должны отправляться к этому пункту в тексте
+
+// -- взять массив пользователей
+// let usersWithAddress = [
+//                 {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//                 {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+//                 {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//                 {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+//                 {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//                 {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+//                 {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+//                 {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//                 {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+//                 {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//                 {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+//             ];
+// Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
+// 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+// 2й - оставляет старше 29 лет включительно
+// 3й - оставляет тех у кого город киев
+// Данные выводить в документ
