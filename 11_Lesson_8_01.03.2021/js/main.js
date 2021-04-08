6e3395978124719392f9852616d3383cfa73f4af
dmkur <fgameua@gmail.com>
2021-04-08 22:09:10 +0300

    edit les 8 again

--- 11_Lesson_8_01.03.2021/js/main.js
+++ 11_Lesson_8_01.03.2021/js/main.js
@@ -1,30 +1,24 @@
-
-
-let div = document.createElement(`div`)
-document.body.appendChild(div)
+let content = document.createElement(`div`)
+document.body.appendChild(content)
 
 fetch('https://api.sampleapis.com/futurama/characters')
-.then(res => res.json())
-
-.then(heroes => {
-    for (const hero of heroes) {
-        //console.log(hero);
+.then(response => response.json())
+.then(item => {
+    for (const hero of item) {
         let info = document.createElement(`div`)
-        let anchor = document.createElement(`a`)
+        let imgHero = document.createElement(`img`)
+        let anchor = document.createElement(`a`)      
+         
         
-        anchor.href = 
-        `11_Lesson_8_01.03.2021/details.html?name=${hero.name.first}&surname=${hero.name.last}` 
+        imgHero.src = `${hero.images.main}`
 
-        anchor.innerText = `${hero.id} - 
-        ${hero.name.first} - 
-        ${hero.name.last}`
+        anchor.innerText = `${hero.id} - ${hero.name.first} - ${hero.name.last}`
+        anchor.href = `#`
 
-
-        let img = document.createElement(`img`)
-        img.src = hero.images.main
 
         info.appendChild(anchor)
-        div.appendChild(info)
-        div.appendChild(img)
+        content.appendChild(info)
+        content.appendChild(imgHero)
+       
     }
 })
